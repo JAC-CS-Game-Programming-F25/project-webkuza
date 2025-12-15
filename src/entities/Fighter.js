39479453e6ba07247659efc248608506955dbaba
AlirezaSJAC <158083430+AlirezaSJAC@users.noreply.github.com>
2025-12-14 23:58:49 -0500

    something before midnight

--- src/entities/Fighter.js
+++ src/entities/Fighter.js
@@ -5,7 +5,7 @@ import InterpretMoveState from "../states/fighter/InterpretMoveState.js";
 
 export default class Fighter extends GameEntity
 {
-    static FIGHTER_WIDTH = 32;
+    static FIGHTER_WIDTH = 64;
     static FIGHTER_HEIGHT = 64;
 
     constructor(x = 0, y = 0, width = 0, height = 0, moveset, direction, isEnemy, health)
@@ -26,7 +26,7 @@ export default class Fighter extends GameEntity
         Object.keys(this.moveset).forEach(key => {
             this.stateMachine.add(key, new InterpretMoveState(this, this.moveset[key]));
         });
-        this.stateMachine.change("idle");
+        this.stateMachine.change("idle", {});
     }
 
     update(dt)
